d29101c1a25affb1ca558482a87e8b7e77c7194b
george zareifhs <giorgoszar67@gmail.com>
2026-08-11 13:22:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -162,3 +162,30 @@ $('.category-btn').on('click', function() {
     // Αρχική φόρτωση
     loadImages();
 });
+
+var darkMode = false;
+
+// default to system setting
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	darkMode = true;
+}
+
+// preference from localStorage should overwrite
+if (localStorage.getItem('theme') === 'dark') {
+	darkMode = true;
+} else if (localStorage.getItem('theme') === 'light') {
+	darkMode = false;
+}
+
+if (darkMode) {
+	document.body.classList.toggle('dark');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementById('theme-toggle').addEventListener('click', () => {
+		document.body.classList.toggle('dark');
+    	localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+	});
+
+});
